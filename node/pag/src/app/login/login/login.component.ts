587804b933dacaa03d8ba7bc4c04d7f8099d2856
Ginubu <gino.d2398@gmail.com>
2022-10-02 11:52:34 -0300

    agrego tarea

--- node/pag/src/app/login/login/login.component.ts
+++ node/pag/src/app/login/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup,Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -12,12 +13,51 @@ export class LoginComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder) { 
     this.form= this.formBuilder.group({
-      password:['',[]],
-      mail: ['',[]],
-    });
+      password:['',[Validators.required,Validators.minLength(8)]],
+      mail: ['',[Validators.required,Validators.email]],
+    })
   }
 
   ngOnInit(): void {
   }
 
+  
+  get Password(){
+    return this.form.get("password");
+  }
+ 
+  get Mail(){
+   return this.form.get("email");
+  }
+
+  get PasswordValid(){
+    return this.Password?.touched && !this.Password?.valid;
+  }
+
+  get MailValid() {
+    return this.Mail?.touched && !this.Mail?.valid;
+  }
+ 
+
+  onEnviar(event: Event){
+    // Detenemos la propagación o ejecución del compotamiento submit de un form
+    event.preventDefault; 
+ 
+    if (this.form.valid){
+      // Llamamos a nuestro servicio para enviar los datos al servidor
+      // También podríamos ejecutar alguna lógica extra
+      alert("Todo salio bien ¡Enviar formuario!")
+    }else{
+      // Corremos todas las validaciones para que se ejecuten los mensajes de error en el template     
+      this.form.markAllAsTouched(); 
+    }
+ 
+  }
+
+
 }
+
+
+
+
+
